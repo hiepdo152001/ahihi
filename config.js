cca4aa1abc94ba7c1f626d0aaf1226b254882d5e
hiepdo152001 <75997961+hiepdo152001@users.noreply.github.com>
2023-07-19 22:41:27 +0700

    Add files via upload
    ahiahi

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = "Quỳnh";
+const giftUrl = "";
+const eventName = "Hoàng Nguyễn Ngọc ";
+const titleCard = "Bất ngờ nè";
+const contentCard = "For the most beautiful";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "hot-girl.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
